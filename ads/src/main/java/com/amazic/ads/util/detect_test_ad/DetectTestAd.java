0ef74e2dfdd0c1b2f7128907dd664d1ff6705540
duongnt1 <@123456>
2024-02-28 09:27:20 +0700

    update detect test ad 28/02/2024

--- ads/src/main/java/com/amazic/ads/util/detect_test_ad/DetectTestAd.java
+++ ads/src/main/java/com/amazic/ads/util/detect_test_ad/DetectTestAd.java
@@ -16,12 +16,28 @@ import com.google.android.gms.vision.text.TextRecognizer;
 
 public class DetectTestAd {
     public static String testAd = "Test Ad";
+    private boolean showAds = false;
+    public static DetectTestAd INSTANCE;
 
-    public static boolean isTestAd(Context context){
+    public static DetectTestAd getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new DetectTestAd();
+        }
+        return INSTANCE;
+    }
+
+    public void setShowAds(boolean showAds, Context context) {
+        this.showAds = showAds;
+        SharedPreferences.Editor editor = context.getSharedPreferences("MY_PRE", Context.MODE_PRIVATE).edit();
+        editor.putBoolean(testAd, !showAds);
+        editor.apply();
+    }
+
+    public boolean isTestAd(Context context) {
         return context.getSharedPreferences("MY_PRE", Context.MODE_PRIVATE).getBoolean(testAd, false);
     }
 
-    public static Bitmap getBitmapFromView(View view) {
+    public Bitmap getBitmapFromView(View view) {
         Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(returnedBitmap);
         Drawable bgDrawable = view.getBackground();
@@ -32,7 +48,8 @@ public class DetectTestAd {
         view.draw(canvas);
         return returnedBitmap;
     }
-    public static String imageToText(Bitmap bitmap, Context context){
+
+    public String imageToText(Bitmap bitmap, Context context, IOnImageToTextSuccess iOnImageToTextSuccess) {
         SharedPreferences.Editor editor = context.getSharedPreferences("MY_PRE", Context.MODE_PRIVATE).edit();
         TextRecognizer textRecognizer = new TextRecognizer.Builder(context.getApplicationContext()).build();
         Frame imageFrame = new Frame.Builder()
@@ -45,10 +62,18 @@ public class DetectTestAd {
             imageText = imageText + "" + textBlock.getValue();
         }
         Log.d("TAG", "imageToText: " + imageText);
-        if (imageText.toLowerCase().contains(testAd.toLowerCase())){
+        if (imageText.toLowerCase().contains(testAd.toLowerCase()) && !this.showAds) {
             editor.putBoolean(testAd, true);
-            editor.commit();
+            if (editor.commit()) {
+                iOnImageToTextSuccess.onImageToTextSuccess();
+            }
+        } else {
+            iOnImageToTextSuccess.onImageToTextSuccess();
         }
         return imageText;
+    }
+
+    public interface IOnImageToTextSuccess {
+        void onImageToTextSuccess();
     }
 }
