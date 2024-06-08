d62026d1e02cecfff841a79cbfb778d29810af7b
toanmt <toanmt@nlbn.net>
2024-06-08 10:15:13 +0700

    update lib loading ads dialog

--- ads/src/main/java/com/amazic/ads/dialog/LoadingAdsDialog.java
+++ ads/src/main/java/com/amazic/ads/dialog/LoadingAdsDialog.java
@@ -4,15 +4,29 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.LayoutRes;
+
 import com.amazic.ads.R;
 
 public class LoadingAdsDialog extends Dialog {
+    private static int layoutLoadingAdsView = -1;
+
+    public static void setLayoutLoadingAdsView(@LayoutRes int layoutLoadingAdsView) {
+        LoadingAdsDialog.layoutLoadingAdsView = layoutLoadingAdsView;
+    }
+
+    public static void resetLayoutLoadingAdsView() {
+        LoadingAdsDialog.layoutLoadingAdsView = -1;
+    }
+
     public LoadingAdsDialog(Context context) {
         super(context, R.style.AppTheme);
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dialog_loading_ads);
+        if (layoutLoadingAdsView == -1) setContentView(R.layout.dialog_loading_ads);
+        else setContentView(layoutLoadingAdsView);
     }
 }
